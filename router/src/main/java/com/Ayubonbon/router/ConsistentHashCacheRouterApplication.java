5d9e991e2584e9a61eb0d2d38307fc0e9fc7252c
ayubonbon <ayushi.7sawant@gmail.com>
2026-01-02 11:27:35 +0530

    initial project setup

---
+++ router/src/main/java/com/Ayubonbon/router/ConsistentHashCacheRouterApplication.java
@@ -0,0 +1,13 @@
+package com.Ayubonbon.router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsistentHashCacheRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsistentHashCacheRouterApplication.class, args);
+	}
+
+}
---
+++ router/src/test/java/com/Ayubonbon/router/ConsistentHashCacheRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ayubonbon.router;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsistentHashCacheRouterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
